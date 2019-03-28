c8ae2965bad1739fcbdfd33a2f7d79d1e537d6b5
AlexeyMalinov <alexeymalinov@yandex.ru>
2019-03-28 12:34:25 +0300

    Доработан класс SelectionSort

--- src/ru/avalon/java/dev/j10/labs/sort/SelectionSort.java
+++ src/ru/avalon/java/dev/j10/labs/sort/SelectionSort.java
@@ -18,8 +18,14 @@ public class SelectionSort implements Sort {
      * {@inheritDoc}
      */
     public void sort(int[] array) {
-        /*
-         * TODO(Студент): Реализовать метод sort класса SelectionSort
-         */
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[min] > array[j]){
+                    min = j;
+                }
+                swap(array,i,min);
+            }
+        }
     }
 }
